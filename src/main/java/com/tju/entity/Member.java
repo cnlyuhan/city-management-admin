e2ca45f9a6de15119bec2afbdd1f7e42ac319587
cnlyuhan <cnlyuhan@163.com>
2023-10-22 16:10:33 +0800

    commit entity.Member

--- src/main/java/com/tju/entity/Member.java
+++ src/main/java/com/tju/entity/Member.java
@@ -1,8 +1,12 @@
-package com.tju.entity;/**
+package com.tju.entity;
+
+import java.io.Serializable;
+
+/**
 * @description: TODO
 * @author Kepass1ove
 * @date 2023/10/22 15:46
 * @version 1.0
 */
-public class Member {
+public class Member implements Serializable {
 }
